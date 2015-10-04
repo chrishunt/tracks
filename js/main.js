82212b90e4e58a186cdff84eb37172c18fc9fa3d
Chris Hunt <c@chrishunt.co>
2015-10-04 14:31:08 -0700

    Use strict in main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
-function filesFromURL () {
-  return window.location.search.replace("?","").split("&");
-}
+(function () {
+  'use strict';
 
-function loadGPX(map, filename) {
-  $.ajax({url: filename,
-    dataType: "xml",
-    success: function(data) {
-      var parser = new GPXParser(data, map);
-      parser.setTrackColour("#ff0000");
-      parser.setTrackWidth(5);
-      parser.setMinTrackPointDelta(0.001);
-      parser.centerAndZoom(data);
-      parser.addTrackpointsToMap();
-      parser.addWaypointsToMap();
+  function filesFromURL () {
+    return window.location.search.replace("?","").split("&");
+  }
+
+  function loadGPX(map, filename) {
+    $.ajax({url: filename,
+      dataType: "xml",
+      success: function(data) {
+        var parser = new GPXParser(data, map);
+        parser.setTrackColour("#ff0000");
+        parser.setTrackWidth(5);
+        parser.setMinTrackPointDelta(0.001);
+        parser.centerAndZoom(data);
+        parser.addTrackpointsToMap();
+        parser.addWaypointsToMap();
+      }
+    });
+  }
+
+  $(document).ready(function() {
+    var files = filesFromURL();
+
+    var mapOptions = {
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+
+    for (var i = 0; i < files.length; i++) {
+      loadGPX(map, "gpx/" + files[i] + ".GPX");
     }
   });
-}
-
-$(document).ready(function() {
-  var files = filesFromURL();
-
-  var mapOptions = {
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(document.getElementById("map"),
-      mapOptions);
-
-  for (var i = 0; i < files.length; i++) {
-    loadGPX(map, "gpx/" + files[i] + ".GPX");
-  }
-});
+}());
