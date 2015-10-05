084849b8fd8c6628ee8825bfc26190d95e8ab701
Chris Hunt <c@chrishunt.co>
2015-10-05 14:25:31 -0700

    Suport date ranges "2015-10-01..2015-10-03"

--- js/map.js
+++ js/map.js
@@ -1,13 +1,43 @@
 (function () {
   'use strict';
 
-  // Returns track filenames and colors from the URL.
+  // Returns track files and colors from the URL.
   //
-  // Url should be in the form:
+  // Tracks can be shown for specific dates and colors:
   //
   //   map.html?2015-09-18&2015-09-20,a000f0
+  //
+  // Or for a date range:
+  //
+  //   map.html?2015-09-18..2015-09-20
   function tracksFromURL () {
-    return window.location.search.replace("?","").split("&");
+    var params = window.location.search.replace("?","").split("&");
+    var tracks = [];
+
+    for (var i = 0; i < params.length; i++) {
+      var track = params[i].split(","),
+          range = parseDateRange(track[0]),
+          color = track[1];
+
+      moment.range(range).by("days", function(date) {
+        tracks.push({
+          file: "gpx/" + date.format("YYYY-MM-DD") + ".gpx",
+          color: "#" + (color || "ff0000"),
+        });
+      });
+    }
+
+    return tracks;
+  }
+
+  // Returns a date range in the form [start, end] given a String:
+  //
+  // "2015-10-03" or "2015-10-01..2015-10-05"
+  function parseDateRange(input) {
+    var dateRange = input.split("..");
+    if(dateRange.length != 2) { dateRange = [dateRange[0], dateRange[0]]; }
+
+    return dateRange;
   }
 
   // Returns layer style in the provided color.
@@ -15,7 +45,7 @@
   // http://leafletjs.com/reference.html#geojson-style
   function customLayer(color) {
     return L.geoJson(null, {
-      style: function(feature) {
+      style: function() {
         return {
           color: color,
           weight: 5,
@@ -30,9 +60,8 @@
   var tracks = tracksFromURL();
 
   for (var i = 0; i < tracks.length; i++) {
-    var track = tracks[i].split(","),
-      file  = "gpx/" + track[0] + ".gpx",
-      color = "#" + (track[1] || "ff0000");
+    var file  = tracks[i].file,
+        color = tracks[i].color;
 
     var runLayer = omnivore.gpx(file, null, customLayer(color))
       .on("ready", function() {
